e61408454b277ef6c751747fb2b5b43176c2fd27
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2024-03-17 12:15:42 +0000

    Update convert_ply_multi.sh

--- convert_ply_multi.sh
+++ convert_ply_multi.sh
@@ -1,5 +1,5 @@
 for file in ../ply/*.ply; do
 	nohup ./pto "$( basename $file )"  > /dev/null 2>&1 &
-	tmp = $(basename "$1" .ply)
+	tmp=$(basename "$1" .ply)
 	nasm -f elf64 $tmp.asm -o $tmp.o
 done;
